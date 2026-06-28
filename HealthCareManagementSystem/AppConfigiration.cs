11280364d1bad31732edcc8c428cb88d1a3b66cd
Omar-Hassaan <omarhassaann2@gmail.com>
2026-06-28 12:16:44 +0300

    Resolve merge conflict

---
+++ HealthCareManagementSystem/AppConfigiration.cs
@@ -0,0 +1,18 @@
+﻿using HealthCareManagementSystem.Repositories;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using static HealthCareManagementSystem.Repositories.IRepository;
+
+
+namespace HealthCareManagementSystem
+{
+    public static class AppConfigiration
+    {
+        public static void RegisterConfig(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.AddScoped<IRepository<ApplicationUserOtp>, Repository<ApplicationUserOtp>>();
+            services.AddScoped<IDbInitializer, DbInitializer>();
+        }
+    }
+}
